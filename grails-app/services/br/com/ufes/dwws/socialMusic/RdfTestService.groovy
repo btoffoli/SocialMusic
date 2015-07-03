9f879665a74b89d0ce82ecbae6b4eaea5024ac95
Bruno R. Toffoli <btoffoli@gmail.com>
2015-07-02 22:01:04 -0300

    First query test in dbpedia

--- grails-app/services/br/com/ufes/dwws/socialMusic/RdfTestService.groovy
+++ grails-app/services/br/com/ufes/dwws/socialMusic/RdfTestService.groovy
@@ -8,47 +8,76 @@ import org.openrdf.model.Statement
 import org.openrdf.model.ValueFactory
 import org.openrdf.model.impl.NamespaceImpl
 import org.openrdf.model.impl.ValueFactoryImpl
+import org.openrdf.query.BindingSet
+import org.openrdf.query.Query
+import org.openrdf.query.QueryLanguage
+import org.openrdf.query.QueryResult
+import org.openrdf.query.TupleQuery
+import org.openrdf.query.TupleQueryResult
+import org.openrdf.repository.Repository
+import org.openrdf.repository.RepositoryConnection
+import org.openrdf.repository.base.RepositoryWrapper
+import org.openrdf.repository.http.HTTPRepository
+import org.openrdf.repository.http.HTTPTupleQuery
+import org.openrdf.repository.sparql.SPARQLRepository
+import org.openrdf.repository.sparql.query.SPARQLTupleQuery
 import org.springframework.web.method.annotation.ModelFactory
 
 @Transactional
 class RdfTestService {
 
+    static final String dbPedia = 'http://dbpedia.org/sparql'
+
     def test1() {
-        ValueFactory factory = ValueFactoryImpl.getInstance();
 
-        URI bob = factory.createURI("http://ta.sandrart.net/-person-2162.rdf");
-        URI name = factory.createURI("http://ta.sandrart.net/-artwork-2854");
-        Literal bobsName = factory.createLiteral("Bob");
-        Statement nameStatement = factory.createStatement(bob, name, bobsName);
+        String queryStr = '''
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+            SELECT ?node, ?name, ?given
+            WHERE {
+            ?node foaf:name ?name .
+            OPTIONAL {
+             ?node foaf:givenName ?given .
+            }
+            } LIMIT 1000
+        '''
+
+        //Repository repo = new SPARQLRepository(dbPedia, '')
+        Repository repo = new HTTPRepository(dbPedia, '')
+        //Repository repo = new TupleRepo Repository(dbPedia, '')
+
+        repo.initialize()
+        RepositoryConnection repoConnection = repo.connection
+
+//        SPARQLTupleQuery query = repoConnection.prepareQuery(QueryLanguage.SPARQL, queryStr)
+        TupleQuery query = repoConnection.prepareQuery(QueryLanguage.SPARQL, queryStr)
+
+        QueryResult result = query.evaluate()
+
+//        for(BindingSet bindSet : result) {
+//            String name = bindSet.getValue('name')
+//            println name
+//            for (String binding : bindSet.getBindingNames()) {
+//                println binding
+//            }
+//        }
+        List<Map<String, Object>> resp = []
+        while (result.hasNext()) {
+            BindingSet bindSet = result.next();
+            String node = bindSet.getValue('node')
+            String name = bindSet.getValue('name')
+            String given = bindSet.getValue('give')
+            resp << [
+                    node: node,
+                    name: name,
+                    given: given
+            ]
+        }
+
+        return resp
 
     }
-
-
-    def test2() {
-        ValueFactory factory = ValueFactoryImpl.getInstance();
-
-        URI bob = factory.createURI("http://ta.sandrart.net/-person-2162.rdf");
-        URI name = factory.createURI("http://ta.sandrart.net/-artwork-2854");
-        Literal bobsName = factory.createLiteral("Bob");
-        Statement nameStatement = factory.createStatement(bob, name, bobsName);
-    }
-
-
-    def test3() {
-        Model model = ModelFactory.createDefaultModel();
-        String myNS = "http://localhost:8080/CDITravel/data/TourPackage/";
-        String grNS = "http://purl.org/goodrelations/v1#";
-        //model.setNsPrefix("gr", grNS);
-        model.setNamespace(new NamespaceImpl('gr', grNS))
-        //Resou
-        Resource grOffering = ResourceFactory.createResource(grNS + "Offering");
-        Resource grPriceSpecification = ResourceFactory.createResource(grNS + "PriceSpecification");
-//        Property gravailabilityStarts = ResourceFactory.createProperty(grNS + "availabilityStarts");
-//        Property gravailabilityEnds = ResourceFactory.createProperty(grNS + "availabilityEnds");
-//        Property grhasPriceSpecification = ResourceFactory.createProperty(grNS + "hasPriceSpecification");
-//        Property grhasCurrencyValue = ResourceFactory.createProperty(grNS + "hasCurrencyValue");
-    }
-
 
 
 }
